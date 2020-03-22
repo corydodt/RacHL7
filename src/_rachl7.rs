d853b69b2651cb37f820392689b8e978ed450c17
Cory Dodt <121705+corydodt@users.noreply.github.com>
2020-03-22 13:42:26 -0700

    add a function just to see if we can

--- src/_rachl7.rs
+++ src/_rachl7.rs
@@ -1,16 +1,27 @@
 use pyo3::prelude::*;
 use pyo3::wrap_pyfunction;
 
+
 #[pyfunction]
 /// Formats the sum of two numbers as string.
 fn parse(a: usize, b: usize) -> PyResult<String> {
     Ok((a + b).to_string())
 }
 
+
+#[pyfunction]
+// hello a world
+fn hellow() -> PyResult<()> {
+    println!("Hello, world!");
+    Ok(())
+}
+
+
 #[pymodule]
 /// A Python module implemented in Rust.
 fn _rachl7(py: Python, m: &PyModule) -> PyResult<()> {
     m.add_wrapped(wrap_pyfunction!(parse))?;
+    m.add_wrapped(wrap_pyfunction!(hellow))?;
 
     Ok(())
 }
